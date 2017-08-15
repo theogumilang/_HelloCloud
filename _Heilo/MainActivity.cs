f76d0f2585bb80b2c04ea6eddb43094002841433
theogumilang <gumilangtheodorus@gmail.com>
2017-08-15 10:36:27 +0700

    Add project files.

---
+++ _Heilo/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using FHSDK;
+
+namespace _Heilo
+{
+    [Activity(Label = "_Heilo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
