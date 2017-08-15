621a46057eed0fbddccea66afe3331e2e6ac7c70
theogumilang <gumilangtheodorus@gmail.com>
2017-08-15 10:59:46 +0700

    Cek

--- _Heilo/MainActivity.cs
+++ _Heilo/MainActivity.cs
@@ -10,12 +10,39 @@ namespace _Heilo
     [Activity(Label = "_Heilo", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private const string Tag = "MainActivity";
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+            await FHClient.Init();
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.button);
+            var result = FindViewById<TextView>(Resource.Id.result);
+            var input = FindViewById<EditText>(Resource.Id.helloTo);
+            button.Click += async delegate
+            {
+                result.Text = "Calling Cloud.....";
+                var response = await FH.Cloud("hello", "GET", null, new Dictionary<string, string>() { { "hello", input.Text } });
+                if (response.Error == null)
+                {
+                    Log.Debug(Tag, "cloudCall - success");
+                    result.Text = (string)response.GetResponseAsDictionary()["msg"];
+                }
+                else
+                {
+                    Log.Debug(Tag, "cloudCall - fail");
+                    Log.Error(Tag, response.Error.Message, response.Error);
+                    result.Text = response.Error.Message;
+                }
+            };
         }
     }
 }
--- _Heilo/Resources/Resource.Designer.cs
+++ _Heilo/Resources/Resource.Designer.cs
@@ -408,8 +408,14 @@ namespace _Heilo
 			// aapt resource value: 0x7f070006
 			public const int auto = 2131165190;
 			
+			// aapt resource value: 0x7f07000b
+			public const int button = 2131165195;
+			
 			// aapt resource value: 0x7f070007
 			public const int dark = 2131165191;
+			
+			// aapt resource value: 0x7f07000a
+			public const int helloTo = 2131165194;
 			
 			// aapt resource value: 0x7f070003
 			public const int icon_only = 2131165187;
@@ -420,8 +426,14 @@ namespace _Heilo
 			// aapt resource value: 0x7f070002
 			public const int none = 2131165186;
 			
+			// aapt resource value: 0x7f07000c
+			public const int result = 2131165196;
+			
 			// aapt resource value: 0x7f070004
 			public const int standard = 2131165188;
+			
+			// aapt resource value: 0x7f070009
+			public const int textView = 2131165193;
 			
 			// aapt resource value: 0x7f070005
 			public const int wide = 2131165189;
